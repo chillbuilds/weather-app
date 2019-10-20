5c45e44261b5f7aa1c9be3501a6337ea8b333036
heliumface770 <heliumface770@gmail.com>
2019-10-19 20:55:22 -0600

    ajax calls to display current weather data

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 var cityInput = "";
+var APIKey = "8bebd070f80dace3c5498124f468dc36";
+var latitude = 0;
+var longitude = 0;
+
+
 $("#hist1").text(JSON.parse(localStorage.getItem("2")));
 $("#hist2").text(JSON.parse(localStorage.getItem("3")));
 $("#hist3").text(JSON.parse(localStorage.getItem("4")));
 $("#hist4").text(JSON.parse(localStorage.getItem("5")));
 $("#hist5").text(JSON.parse(localStorage.getItem("6")));
 $("#hist6").text(JSON.parse(localStorage.getItem("7")));
+
+function cityCall(){
+    var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q="+ cityInput +"&APPID=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
+          var x = response;
+          var tempF = Math.round(x.main.temp*9/5-459.67);
+          $("#cityDisplay").text(cityInput);
+          $("#temp").text("Temp: " + tempF + "°f");
+          $("#humidity").text("Humidity: " + x.main.humidity + "%")
+          $("#windSpeed").text("Wind Speed: " + x.wind.speed.toFixed(1) + " mph")
+          lat = x.coord.lat;
+          lon = x.coord.lon;
+          uvCall();
+})}
+
+function uvCall(){
+    var queryURL =
+  "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&APPID=" + APIKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  .then(function(response) {
+        $("#uvIndex").text("UV Index: " + response.value.toFixed(1));
+  })
+}
+
 
 
 $("#searchBtn").on("click", function() {
@@ -16,9 +53,6 @@ $("#searchBtn").on("click", function() {
   var x6 = JSON.parse(localStorage.getItem("6"));
   var x7 = JSON.parse(localStorage.getItem("7"));
   cityInput = x1;
-  if(x4 === null){
-      alert("null, bruh");
-  }
   localStorage.setItem("1", JSON.stringify(x1));
   localStorage.setItem("2", JSON.stringify(x1));
   localStorage.setItem("3", JSON.stringify(x2));
@@ -26,10 +60,11 @@ $("#searchBtn").on("click", function() {
   localStorage.setItem("5", JSON.stringify(x4));
   localStorage.setItem("6", JSON.stringify(x5));
   localStorage.setItem("7", JSON.stringify(x6));
-  $("#hist1").text(cityInput);
-    $("#hist2").text(x2);
-    $("#hist3").text(x3);
-    $("#hist4").text(x4);
-    $("#hist5").text(x5);
-    $("#hist6").text(x6);
+  $("#hist1").text(x1);
+  $("#hist2").text(x2);
+  $("#hist3").text(x3);
+  $("#hist4").text(x4);
+  $("#hist5").text(x5);
+  $("#hist6").text(x6);
+  cityCall();
 });
